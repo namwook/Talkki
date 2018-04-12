8213a07206d053b45fc98847ff89b538a4308b5c
namwook <nameuk166@gmail.com>
2018-04-12 09:08:41 +0900

    initialize for socket.io tutorial

---
+++ app.ts
@@ -0,0 +1,42 @@
+import * as createError from 'http-errors'
+import * as express from 'express'
+import * as path from 'path'
+import * as cookieParser from 'cookie-parser'
+import * as logger from 'morgan'
+
+import * as indexRouter from './routes/index'
+import * as usersRouter from './routes/users'
+
+export const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/socket.io', express.static(path.join(__dirname, 'node_modules', 'socket.io-client', 'dist')));
+app.use('/jquery.js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.js')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
